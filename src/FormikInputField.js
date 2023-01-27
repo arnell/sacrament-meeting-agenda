6d30530b539d5ed16ab8dbae7fe54ddd0ab07048
arnell <2532819+arnell@users.noreply.github.com>
2023-01-26 23:23:37 -0700

    Fix hymn number input change handler

--- src/FormikInputField.js
+++ src/FormikInputField.js
@@ -2,14 +2,21 @@ import React from 'react';
 import { Field } from 'formik';
 import PropTypes from 'prop-types';
 
-const FormikInputField = ({ type, name, className }) => (
-  <Field as="input" type={type} name={name} className={className} />
+const FormikInputField = ({ type, name, className, onChange }) => (
+  <Field
+    as="input"
+    type={type}
+    name={name}
+    className={className}
+    onChange={onChange}
+  />
 );
 
 FormikInputField.propTypes = {
   type: PropTypes.string.isRequired,
   name: PropTypes.string.isRequired,
   className: PropTypes.string,
+  onChange: PropTypes.func,
 };
 
 export default FormikInputField;
